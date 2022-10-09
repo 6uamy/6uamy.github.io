94874a4daead82df4c350f5cb5006faffc9a2036
Hwang Seong Taek <hst007@naver.com>
2022-10-09 15:37:58 +0900

    feat:ToDo Complete

--- src/todo.js
+++ src/todo.js
@@ -2,15 +2,19 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.getElementById('todo-list');
 
-const toDos = [];
+const TODOS_KEY = 'todos'
+
+let toDos = [];
 
 function saveToDos(){
-    localStorage.setItem('toDos', JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event){
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== Number(li.id));
+    saveToDos(toDos);
 }
 
 function paintToDo(newTodo){
@@ -20,9 +24,10 @@ function paintToDo(newTodo){
     
     button.innerText = 'x'
     button.addEventListener('click', deleteToDo);
+    li.id = newTodo.id;
     li.appendChild(span);
     li.appendChild(button);
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     toDoList.appendChild(li);
 }
 
@@ -30,9 +35,21 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = '';
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    }
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
 toDoForm.addEventListener('submit', handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos){
+    const parsedToDos  = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
