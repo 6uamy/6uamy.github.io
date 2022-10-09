433eb0960481b13cf9fb51d4880e57d984eb91f4
Hwang Seong Taek <hst007@naver.com>
2022-10-09 16:54:27 +0900

    feat:weather

--- src/weather.js
+++ src/weather.js
@@ -1,7 +1,17 @@
+const API_KEY = '2c5995dd91584b1d74bba842cc3394f5';
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const long = position.coords.longitude;
-    console.log(lat, long);
+    const lon = position.coords.longitude;
+    console.log(lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector('#weather span:first-child');
+        const city = document.querySelector('#weather span:last-child');
+
+        city.innerText = data.name;
+        weather.innerText = data.weather[0].main;
+    });
 }
 
 function onGeoError(){
@@ -9,4 +19,4 @@ function onGeoError(){
 }
 
 
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError); 
