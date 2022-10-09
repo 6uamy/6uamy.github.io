edfb986d0aa23761c776ad234cc8150d531d453f
Hwang Seong Taek <hst007@naver.com>
2022-10-09 23:17:26 +0900

    style:delete console.log

--- src/weather.js
+++ src/weather.js
@@ -3,7 +3,6 @@ const API_KEY = '2c5995dd91584b1d74bba842cc3394f5';
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log(lat, lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then(data => {
         const weather = document.querySelector('#weather span:first-child');
