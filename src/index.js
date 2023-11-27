f5a2b106a17f17e06949b895f7ea0da014a5299a
6uamy <hst007@naver.com>
2023-11-27 11:48:57 +0900

    myPortfolio init

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const body = document.querySelector('body');
-
-function randomImg(){
-    const random = Math.floor((Math.random() * 10 ) + 1);
-    body.style.backgroundImage = `url(img/${random}.jpg)`;  
-}
-
-randomImg();
